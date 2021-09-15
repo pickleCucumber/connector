c8489c0f5492e05f64ccc49925ee9ca85fcd73dc
pickleCucumber <63567038+pickleCucumber@users.noreply.github.com>
2021-09-15 17:06:44 +0300

    Update easy_conn_dwh.py

--- easy_conn_dwh.py
+++ easy_conn_dwh.py
@@ -10,7 +10,7 @@ from pandas import read_sql_query
 PASSWORD = getpass.getpass()
 
 ROLE = 'YOUR_NAME'
-DSN_TNS = '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=dome-db.isb)(PORT=1521))(CONNECT_DATA=(SID=DOME)))'
+DSN_TNS = '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=host)(PORT=1521))(CONNECT_DATA=(SID=DOME)))'
 
 
 def upload_data(query, chunksize=10 ** 5):
@@ -24,27 +24,8 @@ def upload_data(query, chunksize=10 ** 5):
     return df_data
 query_get_sample=\
 f"""
-select 
- cl.AGREEMENT_ID
-,(nvl(r.ON_BALANCE_PRINCIPAL_EXPOSURE,0) + nvl(r.NET_ON_BALANCE_INTEREST_EXP,0)*nvl(cl.IFRS_CCF_RATE,0)) as EAD
-,cl.PD_FINAL
-,cl.LGD_FINAL
-,cl.IFRS_DEFAULT_FLG
-,dm.loan_type_id
-FROM dwh.recode_agreement_ss s
-right join DACU.GDS_ON_BALANCE_EXPOSURE r
-On s.ss_01='VBX' and s.ss_02='CFT'
-And r.contract_id=s.ss_01_agreement_rk
-And substr(s.ss_02_agreement_id,1,1)!='-'
-inner join scoring_app.scr_dm_portfolio dm
-on dm.agreement_rk=nvl(s.ss_02_agreement_rk,r.contract_id)
-and dm.report_dt=last_day(to_date('202107', 'yyyymm'))
-inner join RISK_OLAP.HIST_IFRS_CLOSING_AGR cl
-on dm.AGREEMENT_ID = cl.AGREEMENT_ID 
-and dm.REPORT_DT = cl.REPORT_DT
-where r.ON_BALANCE_PRINCIPAL_EXPOSURE + r.NET_ON_BALANCE_INTEREST_EXP > 0
-and ENTRY_DT = 202107
-and cl.SCENARIO_ID = 1
+select *
+from your_table
 
 """
 df = upload_data(query_get_sample)
